57b1428efcd0e1f42ea7f3a23347d5f812f6a499
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-04-21 18:42:53 +0300

    Refactoring

--- sample.py
+++ sample.py
@@ -16,16 +16,16 @@ def open_jobs_list(login, password, job, social_auth_type):
     website = 'https://djinni.co/my/dashboard/'
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(website)
+    wait = WebDriverWait(driver, 5)
 
-    login_to_djinni(driver, login, password, social_auth_type)
-    open_job_list(driver, job)
+    login_to_djinni(driver, wait, login, password, social_auth_type)
+    open_job_list(wait, job)
 
     job_header = []
     job_description = []
     company_location = []
     company_type = []
 
-    wait = WebDriverWait(driver, 5)
     time.sleep(3)
 
     xpath = '//ul[@class="pagination pagination_with_numbers"]//li[last()]//a[not(@aria-disabled)]'
@@ -49,22 +49,20 @@ def add_jobs_to_list(driver, job_header):
     job_items = driver.find_elements(By.CLASS_NAME, "profile")
     for item in job_items:
         job_header.append(item.text)
-    time.sleep(2)
+    time.sleep(1)
 
 
-def open_job_list(driver, job):
-    nav_bar_collapse = driver.find_element(By.XPATH, "//div[@class='collapse navbar-collapse']")
-    nav_bar = nav_bar_collapse.find_element(By.XPATH, "//ul[@class='nav navbar-nav']")
-    nav_bar.find_element(By.XPATH, "./li[2]").click()
-    time.sleep(2)
+def open_job_list(wait, job):
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='collapse navbar-collapse']")))
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='collapse navbar-collapse']//ul[@class='nav "
+                                                     "navbar-nav']//li[2]"))).click()
     job_name = "//a[@href='/jobs/keyword-" + job + "/']"
-    driver.find_element(By.XPATH, job_name).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, job_name))).click()
 
 
-def login_to_djinni(driver, login, password, social_auth_type):
+def login_to_djinni(driver, wait, login, password, social_auth_type):
     social = "//button[@value='" + social_auth_type + "']"
-    driver.find_element(By.XPATH, social).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, social))).click()
     driver.find_element(By.ID, "username").send_keys(login)
     driver.find_element(By.ID, "password").send_keys(password)
-    time.sleep(2)
-    driver.find_element(By.XPATH, "//button[@aria-label='Sign in']").click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Sign in']"))).click()
